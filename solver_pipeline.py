e7a7e6b5ce038d6d6cde6678527f7e38d92643e2
Bo Tang <lucastang1994@gmail.com>
2020-05-15 02:41:01 -0400

    Add script

--- solver_pipeline.py
+++ solver_pipeline.py
@@ -1,10 +1,12 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-from matplotlib import pyplot as plt
-from matplotlib import cm
-from mpl_toolkits.mplot3d import Axes3D
+import os
+import time
+import argparse
 import numpy as np
+import pandas as pd
+
 import heuristics
 import ilp
 import utils
@@ -15,32 +17,67 @@ np.random.seed(23)
 
 if __name__ == "__main__":
 
-    for size in [5, 10]:
+    # set parser
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--size', type=int, default=5)
+    parser.add_argument('--noise', type=float, default=0.0)
+    parser.add_argument('--timelimit', type=int, default=600)
+    parser.add_argument('--type', type=str, default='synth', choices=['synth', 'real'])
 
-        for ind in [3, 4]:
+    # get args
+    args = parser.parse_args()
 
-            for timelimit in [1800]:
+    # create folder
+    if not os.path.isdir('./res'):
+        os.mkdir('./res')
 
-                # choose image and size
-                image = generator.generate_images(size)[ind]
+    # create table
+    df = pd.DataFrame(columns=['time', 'gap', 'obj'])
 
-                # initialize with hueristic method
-                heuristic_graph = heuristics.solve(image, 0.02)
-                heuristic_seg, heuristics_output = utils.graph_to_image(heuristic_graph)
+    # load images
+    images = generator.generate_images(args.size)
 
-                # build ilp
-                model = ilp.build_model(image, 1, 0.5, cycle4=True, cycle8=False, facet=True)
-                model.parameters.timelimit.set(timelimit)
+    # image type
+    if args.type == "synth":
+        index = [0, 1, 2]
+    else:
+        index = [3, 4, 5]
 
-                # warm start
-                ilp.warm_start(model, heuristic_seg)
+    for i in index:
+        print("Loading image {}...".format(i))
+        image = images[i]
+        image = image + args.noise * np.random.normal(loc=0.0, scale=1.0, size=image.shape)
+        image = np.clip(image, 0, 1)
 
-                # solve ilp
-                print("Solving ilp with b&c...")
-                model.solve()
+        tick = time.time()
 
-                gap = model.solution.MIP.get_mip_relative_gap()
-                print("MIP relative gap:", gap)
+        # initialize with hueristic method
+        heuristic_graph = heuristics.solve(image, 0.02)
+        heuristic_seg, heuristics_output = utils.graph_to_image(heuristic_graph)
 
-                obj = model.solution.get_objective_value()
-                print("Objective value:", obj)
+        # build ilp
+        model = ilp.build_model(image, 1, 0.5, cycle4=True, cycle8=False, facet=True)
+        model.parameters.timelimit.set(args.timelimit)
+
+        # warm start
+        ilp.warm_start(model, heuristic_seg)
+
+        # solve ilp
+        print("Solving ilp with b&c...")
+        model.solve()
+
+        tock = time.time()
+        elapse = tock - tick
+        print("Time elpase:", elapse)
+
+        gap = model.solution.MIP.get_mip_relative_gap()
+        print("MIP relative gap:", gap)
+
+        obj = model.solution.get_objective_value()
+        print("Objective value:", obj)
+
+        # add data
+        df = df.append(pd.Series([elapse, gap, obj], index=df.columns), ignore_index=True)
+
+    # save data
+    df.to_csv('./res/log.csv')
